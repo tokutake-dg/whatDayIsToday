0b4b865d53bde207dfeced9a727f73fd2eb0b60c
tokutake-dg <tokutake@designet.co.jp>
2018-02-19 00:38:22 +0900

    変更してみたよ

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-test
+from rocketchat.api import RocketChatAPI
+from pprint import pprint
+
+api = RocketChatAPI(settings={'username': 'test', 'password': 'tokutake',
+                              'domain': 'http://localhost:3000'})
+
+api.send_message('message', 'bot')
